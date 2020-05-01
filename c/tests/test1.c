4f8bc4e15878183352b0068cca46612d60a781a6
Tyler <tylerwayne3@gmail.com>
2020-05-01 18:57:49 +0200

    added AutoMake templates andunittesting

---
+++ c/tests/test1.c
@@ -0,0 +1,26 @@
+/* file minunit_example.c */
+
+#include <stdio.h>
+#include "minunit.h"
+
+int tests_run = 0;
+
+static char* test1() {
+  mu_assert("error, 1 != 1", 1 == 1);
+  return 0;
+}
+
+static char* all_tests() {
+  mu_run_test(test1);
+  return 0;
+}
+
+int main(int argc, char** argv) {
+  char* result = all_tests();
+  if (result != 0) printf("%s\n", result);
+  else printf("ALL TESTS PASSED\n");
+
+  printf("Tests run: %d\n", tests_run);
+
+  return result != 0;
+}
